39fbfe4fdff2efc23950899b9aa5ba9afa777215
Ryan O'Neil <ryan@nextmv.io>
2022-06-06 16:21:59 -0400

    Fix import.

--- hop/run/run.go
+++ hop/run/run.go
@@ -1,8 +1,6 @@
 package run
 
-import (
-	"github.com/nextmv-io/sdk/hop/solve"
-)
+import "github.com/nextmv-io/sdk/hop/solve"
 
 // Run a solver by a handler.
 func Run[T any](handler func(T, solve.Options) (solve.Solver, error)) {
