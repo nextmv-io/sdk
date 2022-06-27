c35606913b5c27bd90ff9214cbc9a4622abfe2df
Dirk Schumacher <dirk@nextmv.io>
2022-06-27 12:25:09 +0200

    Add missing connect call

--- route/by_index.go
+++ route/by_index.go
@@ -3,6 +3,7 @@ package route
 // Indexed creates a ByIndex measure from the given ByPoint measure
 // and wrapping the provided points.
 func Indexed(m ByPoint, points []Point) ByIndex {
+	connect()
 	return indexedFunc(m, points)
 }
 
