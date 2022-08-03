4e10052de6c16a1c1e57b3afc62925412174e228
larsbeck <lars.beckmann@gmail.com>
2022-08-03 10:52:10 +0200

    Use cmd/version/main.go in plugins to get version

--- version.go
+++ version.go
@@ -4,14 +4,12 @@
 package sdk
 
 import (
-	_ "embed"
 	"runtime/debug"
 	"strings"
 )
 
 // This will be needed for examples and tests within this repo only.
-//go:embed VERSION
-var versionFallback string
+var versionFallback string = "v0.16.0-dev.0-5"
 
 // VERSION of Nextmv SDK.
 var VERSION string = getVersion()
