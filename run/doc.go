268a294575f5a05506176beb81aeddb75b523071
larsbeck <lars.beckmann@gmail.com>
2022-08-29 13:53:01 +0200

    Update go.doc

--- run/doc.go
+++ run/doc.go
@@ -1,3 +1,6 @@
-/* Package run...*/
-
+/*
+Package run provides tools for running solvers. When `run.Run` is called by your
+application, the runner configured by the environment variable `NEXTMV_RUNNER`
+will be used. Valid values are `cli` (default) and `http`.
+*/
 package run
