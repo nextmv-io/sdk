3ef7f51b59295f174bb6641777ae67614f672a34
larsbeck <lars.beckmann@gmail.com>
2022-09-22 13:22:34 +0200

    Testing fix

--- connect/connect.go
+++ connect/connect.go
@@ -13,7 +13,7 @@ import (
 
 // Connector connects method definitions with plugins.
 type Connector interface {
-	Connect(any)
+	Connect(*any)
 }
 
 type connector struct {
@@ -33,15 +33,21 @@ func NewConnector(slug, prefix string) Connector {
 }
 
 // Connect connects a method with its implementation.
-func (c connector) Connect(target any) {
-	if _, ok := c.connected[&target]; ok {
+func (c connector) Connect(target *any) {
+	connect(c.connected, c.mtx, c.slug, c.prefix, target)
+}
+
+func connect[T any](connected map[any]struct{},
+	mtx *sync.Mutex, slug, prefix string, target *T,
+) {
+	if _, ok := connected[target]; ok {
 		return
 	}
 
-	c.mtx.Lock()
-	defer c.mtx.Unlock()
+	mtx.Lock()
+	defer mtx.Unlock()
 
-	if _, ok := c.connected[target]; ok {
+	if _, ok := connected[target]; ok {
 		return
 	}
 
@@ -50,6 +56,6 @@ func (c connector) Connect(target any) {
 	fullName := runtime.FuncForPC(pc).Name()
 	split := strings.Split(fullName, ".")
 	name := split[len(split)-1]
-	plugin.Connect(c.slug, fmt.Sprintf("%s%s", c.prefix, name), &target)
-	c.connected[target] = struct{}{}
+	plugin.Connect(slug, fmt.Sprintf("%s%s", prefix, name), target)
+	connected[target] = struct{}{}
 }
