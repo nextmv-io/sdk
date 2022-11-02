8db7a77b02f4a98e3073f40362b6687996b02c15
Nils Beckmann <nils.beckmann@gmail.com>
2022-11-02 12:43:22 +0100

    initial mip budget allocation commit

---
+++ templates/mip-budget-allocation/main.go
@@ -0,0 +1,173 @@
+// package main holds the implementation of the mip budget allocation template.
+package main
+
+import (
+	"math"
+
+	"github.com/nextmv-io/sdk/mip"
+	"github.com/nextmv-io/sdk/run"
+	"github.com/nextmv-io/sdk/store"
+)
+
+// This template demonstrates how to solve a Mixed Integer Programming problem.
+// To solve a mixed integer problem is to optimize a linear objective function
+// of many variables, subject to linear constraints. We demonstrate this by
+// solving a budget allocation problem.
+func main() {
+	run.Run(solver)
+}
+
+// budgetAllocationProblem describes the needed input data to run the model.
+// There is a fixed budget that must not be exceeded, a fixed number of users
+// that may receive incentives, and a description of the available incentives.
+type budgetAllocationProblem struct {
+	Users  []user `json:"users"`
+	Budget int    `json:"budget"`
+}
+
+// incentive contains a Cost, Effect, and Availability.
+type incentive struct {
+	Effect int    `json:"effect"`
+	Cost   int    `json:"cost"`
+	ID     string `json:"id"`
+}
+
+// user represents the user for the problem including its name and a
+// cost/effect pair per available incentive.
+type user struct {
+	ID         string      `json:"id"`
+	Incentives []incentive `json:"incentives"`
+}
+
+// assignments is used to print the solution and represents the
+// combination of a user with the assigned incentive.
+type assignments struct {
+	User        string `json:"user"`
+	IncentiveID string `json:"incentive_id"`
+	Cost        int    `json:"cost"`
+	Effect      int    `json:"effect"`
+}
+
+func solver(
+	input budgetAllocationProblem,
+	opts store.Options,
+) (store.Solver, error) {
+	// We start by creating a MIP model.
+	m := mip.NewModel()
+
+	// We want to maximize the value of the knapsack.
+	m.Objective().SetMaximize()
+
+	// This constraint ensures the budgetConstraint of the knapsack will
+	// not be exceeded.
+	budgetConstraint := m.NewConstraint(mip.LessThanOrEqual, float64(input.Budget))
+
+	userIncentive := make(map[string][]mip.Var, len(input.Users))
+	for _, user := range input.Users {
+		userIncentive[user.ID] = make([]mip.Var, len(user.Incentives))
+		atMostOne := m.NewConstraint(mip.LessThanOrEqual, 1.0)
+		for i, incentive := range user.Incentives {
+			x := m.NewBool()
+			userIncentive[user.ID][i] = x
+			m.Objective().NewTerm(float64(incentive.Effect), x)
+			budgetConstraint.NewTerm(float64(incentive.Cost), x)
+			atMostOne.NewTerm(1, x)
+		}
+	}
+
+	// We create a solver using the 'highs' provider.
+	solver, err := mip.NewSolver("highs", m)
+	if err != nil {
+		return nil, err
+	}
+
+	// We create the solve options we will use.
+	solveOptions := mip.NewSolveOptions()
+
+	// Limit the solve to a maximum duration.
+	err = solveOptions.SetMaximumDuration(opts.Limits.Duration)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the relative gap to 0% (highs' default is 5%).
+	err = solveOptions.SetMIPGapRelative(0)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set verbose level to see a more detailed output.
+	solveOptions.SetVerbosity(mip.Low)
+
+	// We use a store, and it's corresponding Format to report the solution
+	// Doing this allows us to use the CLI runner in the main function.
+	root := store.New()
+
+	// Add initial solution as nil.
+	so := store.NewVar[mip.Solution](root, nil)
+
+	i := 0
+	root = root.Generate(func(s store.Store) store.Generator {
+		return store.Lazy(func() bool {
+			// Only run one state transition in which we solve the mip model.
+			return i == 0
+		}, func() store.Store {
+			i++
+			// Invoke the solver.
+			solution, err := solver.Solve(solveOptions)
+			if err != nil {
+				panic(err)
+			}
+			return s.Apply(so.Set(solution))
+		})
+	}).Validate(func(s store.Store) bool {
+		solution := so.Get(s)
+		if solution == nil {
+			return false
+		}
+		// If the solution has values, accept it as being valid, optionally
+		// write a check to test for actual validity.
+		b := solution.HasValues()
+		return b
+	})
+
+	root = root.Format(func(s store.Store) any {
+		// Get solution from store.
+		solution := so.Get(s)
+
+		report := make(map[string]any)
+		report["status"] = "infeasible"
+		report["runtime"] = solution.RunTime().String()
+
+		if solution.HasValues() {
+			if solution.IsOptimal() {
+				report["status"] = "optimal"
+			} else {
+				report["status"] = "suboptimal"
+			}
+			// We change the output so that it is easier to read.
+			assigned := []assignments{}
+			for i, user := range input.Users {
+				for j := range user.Incentives {
+					if int(math.Round(solution.Value(userIncentive[user.ID][j]))) < 1 {
+						continue
+					}
+					oc := assignments{
+						Cost:        input.Users[i].Incentives[j].Cost,
+						Effect:      input.Users[i].Incentives[j].Effect,
+						User:        user.ID,
+						IncentiveID: input.Users[i].Incentives[j].ID,
+					}
+					assigned = append(assigned, oc)
+				}
+			}
+			report["assignments"] = assigned
+			report["value"] = solution.ObjectiveValue()
+		}
+		return report
+	})
+
+	// We invoke Satisfier which will result in invoking Format and
+	// report the solution.
+	return root.Satisfier(opts), nil
+}
