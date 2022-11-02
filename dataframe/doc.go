7cce818b28392970c30f5735872dbd1976cdb028
David Rijsman <drijsman@nextmv.io>
2022-11-02 12:42:42 +0100

    Dataframe package beta disclaimer

--- dataframe/doc.go
+++ dataframe/doc.go
@@ -1,5 +1,7 @@
 /*
 Package dataframe provides a general interface for managing tabular data
 that support filtering, aggregation and data manipulation.
+
+This package is beta and its API is subject to change.
 */
 package dataframe
