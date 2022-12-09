041ceb10b3d117f8b42fc32641c4ef66b8b47dac
larsbeck <lars.beckmann@gmail.com>
2022-12-09 18:32:58 +0100

    Add support for async callbacks

--- run/http_runner.go
+++ run/http_runner.go
@@ -1,11 +1,13 @@
 package run
 
 import (
+	"bytes"
 	"context"
-	"io"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/nextmv-io/sdk/run/decode"
 	"github.com/nextmv-io/sdk/run/encode"
@@ -20,11 +22,17 @@ type HTTPRunner[Input, Option, Solution any] interface {
 	SetLogger(*log.Logger)
 	// SetMaxParallel sets the maximum number of parallel requests.
 	SetMaxParallel(int)
-	// HandlerToIOProducer configures the IOProducer based on the http request.
-	HandlerToIOProducer(
-		func(w http.ResponseWriter, req *http.Request,
-		) (IOProducer, error))
+	// SetHTTPRequestHandler sets the function that handles the http request.
+	SetHTTPRequestHandler(HTTPRequestHandler)
 }
+
+// Callback is a function that is called after the request is processed.
+type Callback func() error
+
+// HTTPRequestHandler is a function that handles an http request.
+type HTTPRequestHandler func(
+	w http.ResponseWriter, req *http.Request,
+) (Callback, IOProducer, error)
 
 // HTTPRunnerOption configures a HTTPRunner.
 type HTTPRunnerOption[Input, Option, Solution any] func(
@@ -54,14 +62,12 @@ func SetMaxParallel[Input, Option, Solution any](maxParallel int) func(
 	}
 }
 
-// HandlerToIOProducer configures the IOProducer based on the http request.
-func HandlerToIOProducer[Input, Option, Solution any](
-	f func(w http.ResponseWriter, req *http.Request) (IOProducer, error),
-) func(
-	HTTPRunner[Input, Option, Solution],
+// SetHTTPRequestHandler sets the function that handles the http request.
+func SetHTTPRequestHandler[Input, Option, Solution any](
+	f HTTPRequestHandler) func(HTTPRunner[Input, Option, Solution],
 ) {
 	return func(r HTTPRunner[Input, Option, Solution]) {
-		r.HandlerToIOProducer(f)
+		r.SetHTTPRequestHandler(f)
 	}
 }
 
@@ -71,7 +77,7 @@ func NewHTTPRunner[Input, Option, Solution any](
 	options ...HTTPRunnerOption[Input, Option, Solution],
 ) HTTPRunner[Input, Option, Solution] {
 	runner := &httpRunner[Input, Option, Solution]{
-		genericRunner: &genericRunner[Input, Option, Solution]{
+		genericRunner: genericRunner[Input, Option, Solution]{
 			InputDecoder:  NewGenericDecoder[Input](decode.JSON()),
 			OptionDecoder: QueryParamDecoder[Option],
 			Algorithm:     algorithm,
@@ -98,7 +104,7 @@ func NewHTTPRunner[Input, Option, Solution any](
 	}
 
 	// default handler to IOProducer
-	runner.handlerToIOProducer = DefaultHandlerToIOProducer
+	runner.httpRequestHandler = NewHTTPRequestHandler
 
 	for _, option := range options {
 		option(runner)
@@ -108,25 +114,63 @@ func NewHTTPRunner[Input, Option, Solution any](
 }
 
 type httpRunner[Input, Option, Solution any] struct {
-	*genericRunner[Input, Option, Solution]
-	httpServer          *http.Server
-	maxParallel         chan struct{}
-	handlerToIOProducer func(
-		w http.ResponseWriter, req *http.Request,
-	) (IOProducer, error)
+	genericRunner[Input, Option, Solution]
+	httpServer         *http.Server
+	maxParallel        chan struct{}
+	httpRequestHandler HTTPRequestHandler
 }
 
-// DefaultHandlerToIOProducer allows the input and option to be sent as body and
+// NewHTTPRequestHandler allows the input and option to be sent as body and
 // query parameters.
-func DefaultHandlerToIOProducer(
+func NewHTTPRequestHandler(
 	w http.ResponseWriter, req *http.Request,
-) (IOProducer, error) {
-	var writer io.Writer = w
-	return func(ctx context.Context, config any) IOData {
+) (Callback, IOProducer, error) {
+	return nil, func(ctx context.Context, config any) IOData {
 		return NewIOData(
 			req.Body,
 			req.URL.Query(),
-			writer,
+			w,
+		)
+	}, nil
+}
+
+// NewAsyncHTTPRequestHandler creates a new HTTPRequestHandler.
+func NewAsyncHTTPRequestHandler(httpClient *http.Client) HTTPRequestHandler {
+	return asyncHTTPHandler{
+		httpClient: httpClient,
+	}.Handler
+}
+
+type asyncHTTPHandler struct {
+	httpClient *http.Client
+}
+
+func (a asyncHTTPHandler) Handler(
+	_ http.ResponseWriter, req *http.Request,
+) (Callback, IOProducer, error) {
+	callbackURL := req.Header.Get("callback_url")
+	if callbackURL == "" {
+		return nil, nil, errors.New("callback_url not found in header")
+	}
+
+	buf := new(bytes.Buffer)
+	callbackFunc := func() (err error) {
+		resp, err := a.httpClient.Post(callbackURL, "application/json", buf)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if cerr := resp.Body.Close(); cerr != nil {
+				err = cerr
+			}
+		}()
+		return err
+	}
+	return callbackFunc, func(ctx context.Context, config any) IOData {
+		return NewIOData(
+			req.Body,
+			req.URL.Query(),
+			buf,
 		)
 	}, nil
 }
@@ -143,10 +187,10 @@ func (h *httpRunner[Input, Option, Solution]) SetMaxParallel(maxParallel int) {
 	h.maxParallel = make(chan struct{}, maxParallel)
 }
 
-func (h *httpRunner[Input, Option, Solution]) HandlerToIOProducer(
-	f func(w http.ResponseWriter, req *http.Request) (IOProducer, error),
+func (h *httpRunner[Input, Option, Solution]) SetHTTPRequestHandler(
+	f HTTPRequestHandler,
 ) {
-	h.handlerToIOProducer = f
+	h.httpRequestHandler = f
 }
 
 func (h *httpRunner[Input, Option, Solution]) Run(
@@ -169,8 +213,6 @@ func (h *httpRunner[Input, Option, Solution]) ServeHTTP(
 ) {
 	select {
 	case h.maxParallel <- struct{}{}:
-		// We have a free slot, so we can start a new run.
-		defer func() { <-h.maxParallel }()
 	default:
 		// No free slot, so we immediately return an error.
 		http.Error(w, "max number of parallel requests exceeded",
@@ -178,21 +220,48 @@ func (h *httpRunner[Input, Option, Solution]) ServeHTTP(
 		return
 	}
 
-	// configure how to turn the request and response into an IOProducer.
-	producer, err := h.handlerToIOProducer(w, req)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// control mechanism to let the request by run async or not.
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer func() { <-h.maxParallel }()
+		// configure how to turn the request and response into an IOProducer.
+		callbackFunc, producer, err := h.httpRequestHandler(w, req)
+		async := callbackFunc != nil
+		if async {
+			wg.Done()
+		} else {
+			defer wg.Done()
+		}
+		if err != nil {
+			handleError(async, err, w)
+			return
+		}
+		// get a copy of the genericRunner set the IOProducer and run it.
+		genericRunner := h.genericRunner
+		genericRunner.SetIOProducer(producer)
+		err = genericRunner.Run(context.Background())
+		if err != nil {
+			handleError(async, err, w)
+			return
+		}
 
-	h.SetIOProducer(producer)
+		// if the request is async, call the callbackFunc.
+		if async {
+			err = callbackFunc()
+			if err != nil {
+				handleError(async, err, w)
+				return
+			}
+		}
+	}()
+	wg.Wait()
+}
 
-	err = h.genericRunner.Run(context.Background())
-	if err != nil {
-		log.Println(err)
+func handleError(async bool, err error, w http.ResponseWriter) {
+	log.Println(err)
+	if !async {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 }
 
