de99fcf815d93d6221dc6e1504fba556e1c177d9
Nils Beckmann <nils.beckmann@gmail.com>
2023-02-09 13:27:58 +0100

    implements a test for time dependent measures

--- measure/dependent_by_index.go
+++ measure/dependent_by_index.go
@@ -2,6 +2,8 @@ package measure
 
 import (
 	"encoding/json"
+	"fmt"
+	"sort"
 )
 
 // DependentIndexed is a measure uses a custom cost func to calculate parameter
@@ -56,4 +58,43 @@ func (b *dependentIndexed) MarshalJSON() ([]byte, error) {
 		"type":          "dependentIndexed",
 		"timeDependent": b.timeDependent,
 	})
+}
+
+// ByIndexAndTime holds a measure and an endTime up until this measure is to be
+// used. ByIndexAndTime is to be used with NewTimeDependentMeasure which a slice
+// of ByIndexAndTime.
+type ByIndexAndTime struct {
+	m       ByIndex
+	endTime int
+}
+
+type timeDependentMeasures []ByIndexAndTime
+
+// NewTimeDependentMeasure is used to create a time dependent cost function. The
+// passed slice is sorted by endTime first.
+func NewTimeDependentMeasure(measures []ByIndexAndTime) timeDependentMeasures {
+	sort.SliceStable(measures, func(i, j int) bool {
+		return measures[i].endTime < measures[j].endTime
+	})
+	return measures
+}
+
+// TimeDependentCosts returns the cost for connecting to points by index. It
+// selects the measure by looping of the sorted MeasureByTime slice and picks
+// the first measure that satisfies the condition endTime < time. If no such
+// endTime is given, the function panics.
+func (t timeDependentMeasures) TimeDependentCosts() func(
+	from,
+	to int,
+	data VehicleData,
+) float64 {
+	return func(from, to int, data VehicleData) float64 {
+		time := data.Times.EstimatedDeparture[from]
+		for _, measure := range t {
+			if measure.endTime < time {
+				return measure.m.Cost(from, to)
+			}
+		}
+		panic(fmt.Sprintf("no measure for time '%d' was provided", time))
+	}
 }
