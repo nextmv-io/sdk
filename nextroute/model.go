4d2eef517f69f6c3de97ea45e431fb8ed7628598
larsbeck <lars.beckmann@gmail.com>
2023-02-28 09:52:44 +0100

    Add comments

--- nextroute/model.go
+++ nextroute/model.go
@@ -34,10 +34,12 @@ type Model interface {
 	// unit is used to convert distances to values and vice versa. This is
 	// also used for reporting.
 	DistanceUnit() common.DistanceUnit
+
 	// DurationUnit returns the unit of duration used in the model. The
 	// unit is used to convert durations to values and vice versa. This is
 	// also used for reporting.
 	DurationUnit() time.Duration
+
 	// DurationToValue converts the specified duration to a value as it used
 	// internally in the model.
 	DurationToValue(duration time.Duration) float64
@@ -46,6 +48,7 @@ type Model interface {
 	// time.Time to float64 and vice versa. All float64 values are relative
 	// to the epoch.
 	Epoch() time.Time
+
 	// Expressions returns all expressions of the model for which a solution
 	// has to calculate values. The expressions are sorted by their index. The
 	// constraints register their expressions with the model.
@@ -58,18 +61,18 @@ type Model interface {
 	// NewPlanSingleStop creates a new plan single stop. A plan single stop
 	// is a plan cluster of a single stop. A plan cluster is a collection of
 	// stops which are always planned and unplanned as a single entity.
-	NewPlanSingleStop(
-		stop ModelStop,
-	) ModelPlanSingleStop
-	NewStop(
-		location common.Location,
-	) (ModelStop, error)
+	NewPlanSingleStop(stop ModelStop) ModelPlanSingleStop
+
+	// NewStop creates a new stop. The stop is used to create plan clusters.
+	NewStop(location common.Location) (ModelStop, error)
+
 	// NewVehicleType creates a new vehicle type. The vehicle type is used
 	// to create vehicles.
 	NewVehicleType(
 		travelDuration TravelDurationExpression,
 		processDuration DurationExpression,
 	) (ModelVehicleType, error)
+
 	// NumberOfStops returns the number of stops in the model.
 	NumberOfStops() int
 
@@ -86,18 +89,24 @@ type Model interface {
 
 	// SetDistanceUnit sets the distance unit of the model.
 	SetDistanceUnit(distanceUnit common.DistanceUnit)
+
 	// SetDurationUnit sets the duration unit of the model.
 	SetDurationUnit(durationUnit time.Duration)
+
 	// SetEpoch sets the epoch of the model. The epoch is used to convert
 	// time.Time to float64 and vice versa. All float64 values are relative
 	// to the epoch.
 	SetEpoch(epoch time.Time)
+
 	// SetRandom sets the random number generator of the model.
 	SetRandom(random *rand.Rand)
+
 	// SetSeed sets the seed of the random number generator of the model.
 	SetSeed(seed int64)
+
 	// Stops returns all stops of the model.
 	Stops() ModelStops
+
 	// Stop returns the stop with the specified index.
 	Stop(index int) ModelStop
 
@@ -106,6 +115,7 @@ type Model interface {
 
 	// VehicleTypes returns all vehicle types of the model.
 	VehicleTypes() ModelVehicleTypes
+
 	// VehicleType returns the vehicle type with the specified index.
 	VehicleType(index int) ModelVehicleType
 }
