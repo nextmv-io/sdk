fa7d243fc0d741f4eb5f998355d0242b240be39c
Marius Merschformann <marius.merschformann@gmail.com>
2023-06-07 14:50:14 -0400

    Adding custom data retrieval helper func

--- nextroute/schema/input.go
+++ nextroute/schema/input.go
@@ -2,6 +2,8 @@
 package schema
 
 import (
+	"encoding/json"
+	"errors"
 	"time"
 )
 
@@ -100,4 +102,34 @@ type Stop struct {
 type Location struct {
 	Lon float64 `json:"lon"`
 	Lat float64 `json:"lat"`
+}
+
+// ConvertCustomData converts the custom data into the given type. If the
+// conversion fails, an error is returned.
+func ConvertCustomData[T any](data any) (T, error) {
+	// Marshal the data again in order to unmarshal it into the correct type.
+	var b []byte
+	var err error
+	if rawCustomData, ok := data.(map[string]any); ok {
+		// Typically, the custom data is a map.
+		b, err = json.Marshal(rawCustomData)
+		if err != nil {
+			return *new(T), err
+		}
+	} else if rawCustomData, ok := data.([]any); ok {
+		// Try slice, if not map.
+		b, err = json.Marshal(rawCustomData)
+		if err != nil {
+			return *new(T), err
+		}
+	} else {
+		return *new(T), errors.New("CustomData is not a map or slice at core")
+	}
+
+	// Unmarshal the custom data into the given custom type.
+	customData := new(T)
+	if err := json.Unmarshal(b, customData); err != nil {
+		return *new(T), err
+	}
+	return *customData, nil
 }
