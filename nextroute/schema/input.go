38370c906d5e2564049f6b28c5b04759f2ef38dc
Marius Merschformann <marius.merschformann@gmail.com>
2023-06-10 10:50:17 -0400

    Reverting non-functional linter style, adding test

--- nextroute/schema/input.go
+++ nextroute/schema/input.go
@@ -110,26 +110,26 @@ func ConvertCustomData[T any](data any) (T, error) {
 	// Marshal the data again in order to unmarshal it into the correct type.
 	var b []byte
 	var err error
-	var value T
 	if rawCustomData, ok := data.(map[string]any); ok {
 		// Typically, the custom data is a map.
 		b, err = json.Marshal(rawCustomData)
 		if err != nil {
-			return value, err
+			return *new(T), err
 		}
 	} else if rawCustomData, ok := data.([]any); ok {
 		// Try slice, if not map.
 		b, err = json.Marshal(rawCustomData)
 		if err != nil {
-			return value, err
+			return *new(T), err
 		}
 	} else {
-		return value, errors.New("CustomData is not a map or slice at core")
+		return *new(T), errors.New("CustomData is not a map or slice")
 	}
 
 	// Unmarshal the custom data into the given custom type.
+	value := new(T)
 	if err := json.Unmarshal(b, value); err != nil {
-		return value, err
+		return *new(T), err
 	}
-	return value, nil
+	return *value, nil
 }
