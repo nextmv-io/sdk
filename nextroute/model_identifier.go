da5e716a63010ec0565295c96f25706b233749ea
Marius Merschformann <marius.merschformann@gmail.com>
2023-07-13 02:00:03 +0200

    Single Identifier interface

--- nextroute/model_identifier.go
+++ nextroute/model_identifier.go
@@ -4,12 +4,6 @@ package nextroute
 type Identifier interface {
 	// ID returns the identifier of the object.
 	ID() string
-}
-
-// IdentifierWriteable is an interface that can be used for identifying objects.
-// Furthermore, it allows the identifier to be modified.
-type IdentifierWriteable interface {
-	Identifier
 	// SetID sets the identifier of the object.
 	SetID(string)
 }
