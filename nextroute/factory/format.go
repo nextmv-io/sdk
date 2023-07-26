a76dc1d3b5ff42b564994fd97f6d35f6bbe06334
David Rijsman <drijsman@nextmv.io>
2023-07-26 13:25:26 +0200

    Linter in sub dirs.

--- nextroute/factory/format.go
+++ nextroute/factory/format.go
@@ -2,10 +2,10 @@ package factory
 
 import (
 	"context"
-	"github.com/nextmv-io/sdk/connect"
 	"math"
 
 	"github.com/nextmv-io/sdk/alns"
+	"github.com/nextmv-io/sdk/connect"
 	"github.com/nextmv-io/sdk/nextroute"
 	"github.com/nextmv-io/sdk/nextroute/schema"
 	runSchema "github.com/nextmv-io/sdk/run/schema"
