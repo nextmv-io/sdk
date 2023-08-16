d4a3d865e7f3e6a5ba22919c6de638e2dabfb624
Nils Beckmann <nils.beckmann@gmail.com>
2023-08-16 11:44:23 +0200

    adds a test to the fleet converter

---
+++ nextroute/schema/fleet_test.go
@@ -0,0 +1,297 @@
+package schema_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/nextmv-io/sdk/nextroute/schema"
+)
+
+func TestFleetConverter(t *testing.T) {
+	// Read fleet input.
+	fleetInput := schema.FleetInput{}
+	err := json.Unmarshal([]byte(fleetInputData), &fleetInput)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Convert fleet input to nextroute input.
+	input, err := schema.FleetToNextRoute(fleetInput)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Convert nextroute input to json for comparison because struct has
+	// pointers.
+	nextrouteInputGot, err := json.MarshalIndent(input, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Do the same for the expected nextroute input to get rid of formatting.
+	inputWant := schema.Input{}
+	err = json.Unmarshal([]byte(nextrouteInputWant), &inputWant)
+	if err != nil {
+		t.Error(err)
+	}
+
+	nextrouteInputWant, err := json.MarshalIndent(inputWant, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Compare the two jsons as string.
+	if string(nextrouteInputGot) != string(nextrouteInputWant) {
+		t.Errorf("got %s; want %s", string(nextrouteInputGot), string(nextrouteInputWant))
+	}
+}
+
+const fleetInputData = `{
+	"defaults": {
+	  "vehicles": {
+		"start": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"end": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"shift_start": "2021-10-17T09:00:00-06:00",
+		"shift_end": "2021-10-17T11:00:00-06:00",
+		"speed": 20,
+		"capacity": 10,
+		"max_stops": 15,
+		"max_distance": 100000,
+		"max_duration": 50000
+	  },
+	  "stops": {
+		"stop_duration": 120,
+		"unassigned_penalty": 200000,
+		"earliness_penalty": 2,
+		"lateness_penalty": 5,
+		"quantity": -5,
+		"max_wait": 300,
+		"compatibility_attributes": ["A"],
+		"hard_window": ["2021-10-17T09:00:00-06:00", "2021-10-17T10:00:00-06:00"]
+	  }
+	},
+	"stop_groups": [["order-24-pickup-1", "order-24-dropoff"]],
+	  "duration_groups": [
+		{
+		  "group": ["order-24-pickup-1", "order-24-dropoff"],
+		  "duration": 10
+		}
+	  ],
+	"vehicles": [
+	  {
+		"id": "vehicle-1",
+		"capacity": 105,
+		"backlog": ["order-24-pickup-1", "order-24-dropoff"],
+		"compatibility_attributes": ["vehicle-1"],
+		"speed": 14,
+		"max_distance": 200000,
+		"max_duration": 55000,
+		"stop_duration_multiplier": 1,
+		"start": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"end": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"initialization_cost": 2
+	  },
+	  {
+		"id": "vehicle-2",
+		"capacity": 95
+	  }
+	],
+	"stops": [
+	  {
+		"id": "order-1-pickup-1",
+		"compatibility_attributes": ["vehicle-1"],
+		"position": {
+		  "lon": -96.827094,
+		  "lat": 33.004745
+		},
+		"precedes": "order-1-dropoff",
+		"quantity": -27
+	  },
+	  {
+		"id": "order-1-dropoff",
+		"target_time": "2021-10-17T09:45:00-06:00",
+		"position": {
+		  "lon": -96.86074,
+		  "lat": 33.005741
+		},
+		"quantity": 27,
+		"hard_window": ["2021-10-17T09:00:00-06:00", "2021-10-17T10:00:00-06:00"]
+	  },
+	  {
+		"id": "order-24-pickup-1",
+		"position": {
+		  "lon": -96.690515,
+		  "lat": 32.995981
+		},
+		"precedes": "order-24-dropoff",
+		"quantity": -9,
+		"compatibility_attributes": ["vehicle-1"]
+	  },
+	  {
+		"id": "order-24-dropoff",
+		"target_time": "2021-10-17T10:55:00-06:00",
+		"position": {
+		  "lon": -96.783115,
+		  "lat": 32.95055
+		},
+		"quantity": 9,
+		"hard_window": ["2021-10-17T09:30:00-06:00", "2021-10-17T12:00:00-06:00"]
+	  }
+	]
+  }`
+
+const nextrouteInputWant = `{
+	"defaults": {
+	  "vehicles": {
+		"capacity": 10,
+		"start_location": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"end_location": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"speed": 20,
+		"start_time": "2021-10-17T09:00:00-06:00",
+		"end_time": "2021-10-17T11:00:00-06:00",
+		"max_stops": 15,
+		"max_distance": 100000,
+		"max_duration": 50000
+	  },
+	  "stops": {
+		"unplanned_penalty": 200000,
+		"quantity": -5,
+		"start_time_window": [
+		  "2021-10-17T09:00:00-06:00",
+		  "2021-10-17T10:00:00-06:00"
+		],
+		"max_wait": 300,
+		"duration": 120,
+		"early_arrival_time_penalty": 2,
+		"late_arrival_time_penalty": 5
+	  }
+	},
+	"stop_groups": [
+	  [
+		"order-24-pickup-1",
+		"order-24-dropoff"
+	  ]
+	],
+	"duration_groups": [
+	  {
+		"group": [
+		  "order-24-pickup-1",
+		  "order-24-dropoff"
+		],
+		"duration": 10
+	  }
+	],
+	"vehicles": [
+	  {
+		"capacity": 105,
+		"compatibility_attributes": [
+		  "vehicle-1",
+		  "vehicle-1_order-24-pickup-1",
+		  "vehicle-1_order-24-dropoff"
+		],
+		"max_distance": 200000,
+		"stop_duration_multiplier": 1,
+		"end_location": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"speed": 14,
+		"max_duration": 55000,
+		"activation_penalty": 0,
+		"start_location": {
+		  "lon": -96.659222,
+		  "lat": 33.122746
+		},
+		"initial_stops": [
+		  {
+			"fixed": false,
+			"id": "order-24-pickup-1"
+		  },
+		  {
+			"fixed": false,
+			"id": "order-24-dropoff"
+		  }
+		],
+		"id": "vehicle-1"
+	  },
+	  {
+		"capacity": 95,
+		"compatibility_attributes": [],
+		"activation_penalty": 0,
+		"initial_stops": [],
+		"id": "vehicle-2"
+	  }
+	],
+	"stops": [
+	  {
+		"precedes": "order-1-dropoff",
+		"quantity": -27,
+		"compatibility_attributes": [
+		  "vehicle-1"
+		],
+		"id": "order-1-pickup-1",
+		"location": {
+		  "lon": -96.827094,
+		  "lat": 33.004745
+		}
+	  },
+	  {
+		"quantity": 27,
+		"start_time_window": [
+		  "2021-10-17T09:00:00-06:00",
+		  "2021-10-17T10:00:00-06:00"
+		],
+		"compatibility_attributes": [],
+		"target_arrival_time": "2021-10-17T09:45:00-06:00",
+		"id": "order-1-dropoff",
+		"location": {
+		  "lon": -96.86074,
+		  "lat": 33.005741
+		}
+	  },
+	  {
+		"precedes": "order-24-dropoff",
+		"quantity": -9,
+		"compatibility_attributes": [
+		  "vehicle-1_order-24-pickup-1"
+		],
+		"id": "order-24-pickup-1",
+		"location": {
+		  "lon": -96.690515,
+		  "lat": 32.995981
+		}
+	  },
+	  {
+		"quantity": 9,
+		"start_time_window": [
+		  "2021-10-17T09:30:00-06:00",
+		  "2021-10-17T12:00:00-06:00"
+		],
+		"compatibility_attributes": [],
+		"target_arrival_time": "2021-10-17T10:55:00-06:00",
+		"id": "order-24-dropoff",
+		"location": {
+		  "lon": -96.783115,
+		  "lat": 32.95055
+		}
+	  }
+	]
+  }`
