51fbb9451e29c5254a1e3d20d793d413924ce90c
larsbeck <lars.beckmann@gmail.com>
2023-11-02 16:00:27 +0100

    Add comments

--- types/duration.go
+++ types/duration.go
@@ -1,9 +1,15 @@
+/*
+Package types provides types.
+*/
 package types
 
 import "time"
 
+// Duration is a wrapper around time.Duration that implements json.Marshaler.
 type Duration time.Duration
 
+// MarshalJSON implements json.Marshaler. It calls time.Duration.String() to
+// serialize the duration.
 func (d Duration) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + time.Duration(d).String() + `"`), nil
 }
