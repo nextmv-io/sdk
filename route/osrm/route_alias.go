735050ac36272f1cabf95f1a54b901e050f30c7c
Nils Beckmann <nils.beckmann@gmail.com>
2023-11-30 10:29:59 +0100

    adds Deprecated comment

--- route/osrm/route_alias.go
+++ route/osrm/route_alias.go
@@ -8,6 +8,9 @@ import (
 // Polyline requests polylines for the given points. The first parameter returns
 // a polyline from start to end and the second parameter returns a list of
 // polylines, one per leg.
+//
+// Deprecated: This package is deprecated and will be removed in a future.
+// Use [github.com/nextmv-io/sdk/measure/osrm] instead.s
 func Polyline(
 	c Client, points []route.Point,
 ) (string, []string, error) {
