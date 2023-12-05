bd9f735789fa81b46fda8107c745a19b670dbfe5
larsbeck <lars.beckmann@gmail.com>
2023-12-05 14:39:17 +0100

    Add comment

--- nextroute/solver.go
+++ nextroute/solver.go
@@ -14,6 +14,7 @@ func NewSolverFactory() alns.NewSolverFactory[Solution] {
 	return newSolverFactory
 }
 
+// NewSolveOptionsFactory creates a new SolveOptionsFactory.
 func NewSolveOptionsFactory() alns.NewSolveOptionsFactory[Solution] {
 	connect.Connect(con, &newSolveOptionsFactory)
 	return newSolveOptionsFactory
