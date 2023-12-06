35f2f9e98c411cd52ea9205515fd03f3fcfa581a
larsbeck <lars.beckmann@gmail.com>
2023-12-06 10:31:44 +0100

    Refactor SolveOptions struct in solve_options.go, remove SolutionBufferSize and set it internally

--- alns/solve_options.go
+++ alns/solve_options.go
@@ -6,13 +6,11 @@ package alns
 //   - solution_buffer_size: 100
 func NewSolveOptions() SolveOptions {
 	return SolveOptions{
-		Iterations:         -1,
-		SolutionBufferSize: 100,
+		Iterations: -1,
 	}
 }
 
 // SolveOptions contains the options for the solve process.
 type SolveOptions struct {
-	Iterations         int `json:"iterations" default:"-1"`
-	SolutionBufferSize int `json:"solution_buffer_size" default:"100"`
+	Iterations int `json:"iterations" default:"-1"`
 }
