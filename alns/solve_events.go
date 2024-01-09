53ea693061d2525fa2081d84d316b445d77126cf
David Rijsman <drijsman@nextmv.io>
2024-01-09 18:23:03 +0100

    Extended event to be able to have more than one argument.

--- alns/solve_events.go
+++ alns/solve_events.go
@@ -6,28 +6,28 @@ import (
 
 // SolveEvents is a struct that contains events that are fired during the solve.
 type SolveEvents[T Solution[T]] struct {
-	OperatorExecuting *events.BaseEvent[SolveInformation[T]]
-	OperatorExecuted  *events.BaseEvent[SolveInformation[T]]
-	ImprovementFound  *events.BaseEvent[SolveInformation[T]]
-	Iterating         *events.BaseEvent[SolveInformation[T]]
-	Iterated          *events.BaseEvent[SolveInformation[T]]
-	ContextDone       *events.BaseEvent[SolveInformation[T]]
-	Start             *events.BaseEvent[SolveInformation[T]]
-	Reset             *events.BaseEvent[SolveInformation[T]]
-	Done              *events.BaseEvent[SolveInformation[T]]
+	OperatorExecuting *events.BaseEvent1[SolveInformation[T]]
+	OperatorExecuted  *events.BaseEvent1[SolveInformation[T]]
+	ImprovementFound  *events.BaseEvent1[SolveInformation[T]]
+	Iterating         *events.BaseEvent1[SolveInformation[T]]
+	Iterated          *events.BaseEvent1[SolveInformation[T]]
+	ContextDone       *events.BaseEvent1[SolveInformation[T]]
+	Start             *events.BaseEvent1[SolveInformation[T]]
+	Reset             *events.BaseEvent2[Solution[T], SolveInformation[T]]
+	Done              *events.BaseEvent1[SolveInformation[T]]
 }
 
 // NewSolveEvents creates a new instance of SolveEvents.
 func NewSolveEvents[T Solution[T]]() SolveEvents[T] {
 	return SolveEvents[T]{
-		OperatorExecuting: &events.BaseEvent[SolveInformation[T]]{},
-		OperatorExecuted:  &events.BaseEvent[SolveInformation[T]]{},
-		ImprovementFound:  &events.BaseEvent[SolveInformation[T]]{},
-		Iterating:         &events.BaseEvent[SolveInformation[T]]{},
-		Iterated:          &events.BaseEvent[SolveInformation[T]]{},
-		ContextDone:       &events.BaseEvent[SolveInformation[T]]{},
-		Start:             &events.BaseEvent[SolveInformation[T]]{},
-		Reset:             &events.BaseEvent[SolveInformation[T]]{},
-		Done:              &events.BaseEvent[SolveInformation[T]]{},
+		OperatorExecuting: &events.BaseEvent1[SolveInformation[T]]{},
+		OperatorExecuted:  &events.BaseEvent1[SolveInformation[T]]{},
+		ImprovementFound:  &events.BaseEvent1[SolveInformation[T]]{},
+		Iterating:         &events.BaseEvent1[SolveInformation[T]]{},
+		Iterated:          &events.BaseEvent1[SolveInformation[T]]{},
+		ContextDone:       &events.BaseEvent1[SolveInformation[T]]{},
+		Start:             &events.BaseEvent1[SolveInformation[T]]{},
+		Reset:             &events.BaseEvent2[Solution[T], SolveInformation[T]]{},
+		Done:              &events.BaseEvent1[SolveInformation[T]]{},
 	}
 }
--- events/base.go
+++ events/base.go
@@ -1,22 +1,44 @@
 // Package events provides a simple event system for the ALNS.
 package events
 
-// BaseEvent is a base event type that can be used to implement events.
-type BaseEvent[T any] struct {
-	handlers []Handler[T]
+// BaseEvent1 is a base event type that can be used to implement events
+// with one payload.
+type BaseEvent1[T any] struct {
+	handlers []Handler1[T]
 }
 
 // Register adds an event handler for this event.
-func (e *BaseEvent[T]) Register(handler Handler[T]) {
+func (e *BaseEvent1[T]) Register(handler Handler1[T]) {
 	e.handlers = append(e.handlers, handler)
 }
 
 // Trigger sends out an event with the payload.
-func (e *BaseEvent[T]) Trigger(payload T) {
+func (e *BaseEvent1[T]) Trigger(payload T) {
 	for _, handler := range e.handlers {
 		handler(payload)
 	}
 }
 
-// Handler is a function that handles an event.
-type Handler[T any] func(payload T)
+// Handler1 is a function that handles an event with one payload.
+type Handler1[T any] func(payload T)
+
+// BaseEvent2 is a base event type that can be used to implement events
+// with two payloads.
+type BaseEvent2[S any, T any] struct {
+	handlers []Handler2[S, T]
+}
+
+// Register adds an event handler for this event.
+func (e *BaseEvent2[S, T]) Register(handler Handler2[S, T]) {
+	e.handlers = append(e.handlers, handler)
+}
+
+// Trigger sends out an event with the payload.
+func (e *BaseEvent2[S, T]) Trigger(payload1 S, payload2 T) {
+	for _, handler := range e.handlers {
+		handler(payload1, payload2)
+	}
+}
+
+// Handler2 is a function that handles an event with two payloads.
+type Handler2[S any, T any] func(payload1 S, payload2 T)
