2eb6ad1cbc6d00e4bd4bae8f98a62d01896769bd
David Rijsman <drijsman@nextmv.io>
2024-01-23 10:32:34 +0100

    Define capacity objectives with strings. Fix capacity objective offset. Better error handling in case custom constraint return nil as position hint.

--- nextroute/factory/model.go
+++ nextroute/factory/model.go
@@ -25,25 +25,27 @@ func NewModel(
 type Options struct {
 	Constraints struct {
 		Disable struct {
-			Attributes         bool `json:"attributes" usage:"ignore the compatibility attributes constraint"`
-			Capacity           bool `json:"capacity" usage:"ignore the capacity constraint"`
-			DistanceLimit      bool `json:"distance_limit" usage:"ignore the distance limit constraint"`
-			Groups             bool `json:"groups" usage:"ignore the groups constraint"`
-			MaximumDuration    bool `json:"maximum_duration" usage:"ignore the maximum duration constraint"`
-			MaximumStops       bool `json:"maximum_stops" usage:"ignore the maximum stops constraint"`
-			MaximumWaitStop    bool `json:"maximum_wait_stop" usage:"ignore the maximum stop wait constraint"`
-			MaximumWaitVehicle bool `json:"maximum_wait_vehicle" usage:"ignore the maximum vehicle wait constraint"`
-			MixingItems        bool `json:"mixing_items" usage:"ignore the do not mix items constraint"`
-			Precedence         bool `json:"precedence" usage:"ignore the precedence (pickups & deliveries) constraint"`
-			VehicleStartTime   bool `json:"vehicle_start_time" usage:"ignore the vehicle start time constraint"`
-			VehicleEndTime     bool `json:"vehicle_end_time" usage:"ignore the vehicle end time constraint"`
-			StartTimeWindows   bool `json:"start_time_windows" usage:"ignore the start time windows constraint"`
+			Attributes         bool     `json:"attributes" usage:"ignore the compatibility attributes constraint"`
+			Capacity           bool     `json:"capacity" usage:"ignore the capacity constraint for all resources"`
+			Capacities         []string `json:"capacities" usage:"ignore the capacity constraint for the given resource names"`
+			DistanceLimit      bool     `json:"distance_limit" usage:"ignore the distance limit constraint"`
+			Groups             bool     `json:"groups" usage:"ignore the groups constraint"`
+			MaximumDuration    bool     `json:"maximum_duration" usage:"ignore the maximum duration constraint"`
+			MaximumStops       bool     `json:"maximum_stops" usage:"ignore the maximum stops constraint"`
+			MaximumWaitStop    bool     `json:"maximum_wait_stop" usage:"ignore the maximum stop wait constraint"`
+			MaximumWaitVehicle bool     `json:"maximum_wait_vehicle" usage:"ignore the maximum vehicle wait constraint"`
+			MixingItems        bool     `json:"mixing_items" usage:"ignore the do not mix items constraint"`
+			Precedence         bool     `json:"precedence" usage:"ignore the precedence (pickups & deliveries) constraint"`
+			VehicleStartTime   bool     `json:"vehicle_start_time" usage:"ignore the vehicle start time constraint"`
+			VehicleEndTime     bool     `json:"vehicle_end_time" usage:"ignore the vehicle end time constraint"`
+			StartTimeWindows   bool     `json:"start_time_windows" usage:"ignore the start time windows constraint"`
 		} `json:"disable"`
 		Enable struct {
 			Cluster bool `json:"cluster" usage:"enable the cluster constraint"`
 		} `json:"enable"`
 	} `json:"constraints"`
 	Objectives struct {
+		Capacities               string  `json:"capacity" usage:"capacity objective, provide triple for each resource 'name:default;factor:1.0;offset;0.0'" default:""`
 		MinStops                 float64 `json:"min_stops" usage:"factor to weigh the min stops objective" default:"1.0"`
 		EarlyArrivalPenalty      float64 `json:"early_arrival_penalty" usage:"factor to weigh the early arrival objective" default:"1.0"`
 		LateArrivalPenalty       float64 `json:"late_arrival_penalty" usage:"factor to weigh the late arrival objective" default:"1.0"`
@@ -52,7 +54,6 @@ type Options struct {
 		VehiclesDuration         float64 `json:"vehicles_duration" usage:"factor to weigh the vehicles duration objective" default:"1.0"`
 		UnplannedPenalty         float64 `json:"unplanned_penalty" usage:"factor to weigh the unplanned objective" default:"1.0"`
 		Cluster                  float64 `json:"cluster" usage:"factor to weigh the cluster objective" default:"0.0"`
-		Capacity                 float64 `json:"capacity" usage:"factor to weigh the capacity objective" default:"0.0"`
 	} `json:"objectives"`
 	Properties struct {
 		Disable struct {
