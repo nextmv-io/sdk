9fabb43cb079efbd887de30a34d056607c53adaa
David Rijsman <drijsman@nextmv.io>
2024-01-30 16:59:50 +0100

    Type alias for backward compatibility.

---
+++ alns/alias.go
@@ -0,0 +1,9 @@
+package alns
+
+import "github.com/nextmv-io/sdk/nextroute"
+
+// ProgressionEntry in alns for backwards compatibility
+type ProgressionEntry nextroute.ProgressionEntry
+
+// Progressioner in alns for backwards compatibility
+type Progressioner nextroute.Progressioner
