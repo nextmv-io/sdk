e40f05e92a7914d96f8b4c6ba2983b064b101980
Marius Merschformann <marius.merschformann@gmail.com>
2024-03-14 17:21:30 +0100

    Re-add detailed version

--- version.go
+++ version.go
@@ -5,9 +5,37 @@ package sdk
 
 import (
 	_ "embed"
+	"runtime/debug"
+	"strings"
 )
 
 // VERSION of Nextmv SDK.
 //
 //go:embed VERSION
-var VERSION string
+var versionFallback string
+
+// VERSION of Nextmv SDK.
+var VERSION = getVersion()
+
+func getVersion() string {
+	// Get version from module dependency.
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		// If this happens, we're not running in a module context. We default to
+		// returning the fallback version.
+		return versionFallback
+	}
+
+	for _, dep := range bi.Deps {
+		// We only care about this repo being used as a dependency.
+		if !strings.HasPrefix(dep.Path, "github.com/nextmv-io/sdk") {
+			continue
+		}
+
+		return dep.Version
+	}
+
+	// If this happens, we're running in a module in which sdk is not a
+	// dependency.
+	return versionFallback
+}
