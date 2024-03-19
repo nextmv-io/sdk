b05d4ad9b024dd99149eb9bac1c389b5875cadee
Nils Beckmann <nils.beckmann@gmail.com>
2024-03-19 12:21:43 +0100

    adds tests for docs

---
+++ run/tests/http/async/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/nextmv-io/sdk/golden"
+)
+
+func TestMain(m *testing.M) {
+	golden.Setup()
+	code := m.Run()
+	golden.Teardown()
+	os.Exit(code)
+}
+
+// TestGoldenBash executes a golden file test, where the bash file is run and
+// the output is compared against the expected one.
+func TestGoldenBash(t *testing.T) {
+	// Execute the rest of the bash commands.
+	golden.BashTest(t, "./bash", golden.BashConfig{
+		DisplayStdout: true,
+		DisplayStderr: true,
+		OutputProcessConfig: golden.OutputProcessConfig{
+			// // TODO: Ignoring output of this test, since it's not stable at this
+			// // point.
+			// AlwaysUpdate: true,
+			VolatileDataFiles: []string{
+				"callback.txt",
+				"callback.json",
+			},
+		},
+	})
+}
