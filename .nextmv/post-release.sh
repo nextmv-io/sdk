e1b9bb3b94ac3efe68a42b65b5addc77b5fbbc98
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2025-07-29 15:01:29 -0500

    Updates nested module bumping
    Makes script robust and simpler.
    
    Uses relative path to config.
    Adds modules' go files to git.

--- .nextmv/post-release.sh
+++ .nextmv/post-release.sh
@@ -1,4 +1,4 @@
-export MODULES=$(cat workflow-configuration.yml | yq '.nested_modules[]' -r)
+export MODULES=$(cat "$(dirname "$0")/workflow-configuration.yml" | yq '.nested_modules[]' -r)
 
 git checkout -b feature/bump-nested-$VERSION
 
@@ -7,10 +7,10 @@ for module in $MODULES; do
     pushd ../$module
     go get github.com/nextmv-io/sdk@$VERSION
     go mod tidy
+    git add go.mod go.sum
     popd
 done
 
-git add --all
 git commit -S -m "Bump nested modules after $VERSION release"
 git push origin --set-upstream feature/bump-nested-$VERSION
 
