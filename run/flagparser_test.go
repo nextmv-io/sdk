ad1230a3f4e7f2624ddc7c7559ee684ed1873baa
Marius Merschformann <marius.merschformann@gmail.com>
2025-10-03 20:28:53 +0200

    Deactivate line length linting for test struct

--- run/flagparser_test.go
+++ run/flagparser_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/nextmv-io/sdk/run"
 )
 
+//nolint:lll
 type ParallelSolveOptions struct {
 	Iterations           int           `json:"iterations"  usage:"maximum number of iterations, -1 assumes no limit; iterations are counted after start solutions are generated" default:"-1"`
 	Duration             time.Duration `json:"duration" usage:"maximum duration of the solver" default:"5s"`
